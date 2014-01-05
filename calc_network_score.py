70a6c52e3e0f0f0321938fac5c380d2b4b04c95b
katryo <katoryo55@gmail.com>
2014-01-05 18:25:12 +0900

    2回以上、上位タスクが出てくるタスクをprintする実験を始める

--- calc_network_score.py
+++ calc_network_score.py
@@ -1,66 +1,16 @@
 # -*- coding: utf-8 -*-
-import networkx as nx
-import pdb
 import utils
-from sqlite_data_loader import SQLiteDataLoader
-import constants
+from graph_task_mapper import GraphTaskMapper
 
 
 if __name__ == '__main__':
-    g = nx.DiGraph()
     pages = utils.load_fetched_pages()
-    sqldl = SQLiteDataLoader()
-    entailment_dictionaries = utils.load_entailment_dictionaries()
+    gtm = GraphTaskMapper()
 
     for page in pages[:20]:
         for task in page.tasks:
-            # まずオリジナルのノードを追加
-            g.add_node('%s_%s' % (task.object_term.name, task.predicate_term))
-            # print('%s_%s' % (task.object_term.name, task.predicate_term))
-
-            # object_term.core_nounのhypohypeを探す
-            hypes = sqldl.select_hypes_with_hypo(task.object_term.name)
-            # hypos = sqldl.select_hypos_with_hype(task.object_term.name)
-
-            entailing_predicates = []
-            for filename in constants.ENTAILMENT_DICTIONARY_NAMES:
-                if task.predicate_term in entailment_dictionaries[filename + '_entailing']:
-                    entailings = entailment_dictionaries[filename + '_entailing'][task.predicate_term]
-                    entailing_predicates.extend(list(entailings))
-
-            entailed_predicates = []
-            for filename in constants.ENTAILMENT_DICTIONARY_NAMES:
-                if task.predicate_term in entailment_dictionaries[filename + '_entailed']:
-                    entaileds = entailment_dictionaries[filename + '_entailed'][task.predicate_term]
-                    entailed_predicates.extend(list(entaileds))
-
-            # 4種類の上位語・下位語が揃った。
-            for hype_or_hype_or_original in (hypes + [task.object_term.name]):
-                for entailing_or_entailed_or_original in (entailing_predicates
-                                                          + entailed_predicates + [task.predicate_term]):
-                    if hype_or_hype_or_original == '収録曲':
-                        continue
-                    g.add_node('%s_%s' % (hype_or_hype_or_original, entailing_or_entailed_or_original))
-                    # オリジナルのノードから、上位・下位に貼る。自分自身にも貼っている。
-                    g.add_edge('%s_%s' %
-                               (task.object_term.name, task.predicate_term),
-                               '%s_%s' %
-                               (hype_or_hype_or_original, entailing_or_entailed_or_original))
+            gtm.add_node_and_edge_with_task(task)
     print('added all edges!')
-    results = g.in_degree()
-    print([(result + str(results[result])) for result in results if results[result] > 1])
-
-'''
-    results = nx.pagerank(g)
-    for result in results:
-        #print(result + str(results[result]))
-        if results[result] > 9.6e-06:
-            print(result + str(results[result]))
-            '''
-    # print([(result + str(results[result]) for result in results if results[result] > 0.1)])
-    # 1エッジで4.1秒
-    # 1ノードにつき10エッジで16秒
-    # 20エッジで32秒。線形ぽい。
-    # おそらく200エッジで300秒ほど。つまり5分。
-    # 1000エッジで1500秒 = 25分。まあいけそう。いざとなったらEC2借りよう。
+    results = gtm.broader_nodes_with_higher_in_degree_score()
+    print(results)
 
--- graph_task_mapper.py
+++ graph_task_mapper.py
@@ -20,6 +20,7 @@ class GraphTaskMapper():
     # object_term.core_nounのhypohypeを探る
         sqldl = SQLiteDataLoader()
         hypes = sqldl.select_hypes_with_hypo(task.object_term.name)
+        return hypes
 
     def entailing_preds(self, task):
         entailing_predicates = []
@@ -27,6 +28,7 @@ class GraphTaskMapper():
             if task.predicate_term in self.entailment_dictionaries[filename + '_entailing']:
                 entailings = self.entailment_dictionaries[filename + '_entailing'][task.predicate_term]
                 entailing_predicates.extend(list(entailings))
+        return entailing_predicates
 
     def entailed_preds(self, task):
         entailed_predicates = []
@@ -34,13 +36,44 @@ class GraphTaskMapper():
             if task.predicate_term in self.entailment_dictionaries[filename + '_entailed']:
                 entaileds = self.entailment_dictionaries[filename + '_entailed'][task.predicate_term]
                 entailed_predicates.extend(list(entaileds))
+        return entailed_predicates
 
     def add_new_node(self, object_term, predicate_term):
-        g.add_node('%s_%s' % (object_term, predicate_term))
+        self.graph.add_node('%s_%s' % (object_term, predicate_term))
             # オリジナルのノードから、上位・下位に貼る。自分自身にも貼っている。
 
     def add_new_edge(self, task, object_term, predicate_term):
-        g.add_edge('%s_%s' %
-                   (task.object_term.name, task.predicate_term),
-                   '%s_%s' %
-                   (object_term, predicate_term))
+        self.graph.add_edge('%s_%s' %
+                            (task.object_term.name, task.predicate_term),
+                            '%s_%s' %
+                            (object_term, predicate_term))
+
+    def in_degree(self):
+        return self.graph.in_degree()
+
+    def add_node_and_edge_with_task(self, task):
+        hypes = self.hypes(task)
+        entailing_predicates = self.entailing_preds(task)
+        entailed_predicates = self.entailed_preds(task)
+
+        # 上位語・下位語が揃った。
+        for hype_or_original in (hypes + [task.object_term.name]):
+            for entailing_or_entailed_or_original in (entailing_predicates +
+                                                      entailed_predicates +
+                                                      [task.predicate_term]):
+                if hype_or_original == '収録曲':
+                    continue
+                self.add_new_node(hype_or_original, entailing_or_entailed_or_original)
+                self.add_new_edge(task, hype_or_original, entailing_or_entailed_or_original)
+
+    def nodes_with_higher_in_degree_score(self):
+        scores = self.in_degree()
+        # ['機能_作る', '理念_よる']
+        return [score for score in scores if scores[score] > 1]
+
+    def broader_nodes_with_higher_in_degree_score(self):
+        high_score_nodes = self.nodes_with_higher_in_degree_score()
+        results = set()
+        for node in high_score_nodes:
+            results.add(self.graph.predecessors(node))
+        return results
